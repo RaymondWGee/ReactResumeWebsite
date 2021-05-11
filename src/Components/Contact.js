a880b537637c9f27c8920b07b8aa9f37039cb0f0
Raymond W Gee <rgee5@mail.sfsu.edu>
2021-05-10 20:34:45 -0700

    reverted to working email commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import emailjs from 'emailjs-com';
 
 class Contact extends Component {
+<<<<<<< HEAD
    render() {
  
      if(this.props.data){
@@ -84,3 +84,94 @@ class Contact extends Component {
  }
  
  export default Contact;
+=======
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var email = this.props.data.email;
+      var message = this.props.data.contactmessage;
+    }
+
+    return (
+      <section id="contact">
+
+         <div className="row section-head">
+
+            <div className="two columns header-col">
+
+               <h1><span>Get In Touch.</span></h1>
+
+            </div>
+
+            <div className="ten columns">
+
+                  <p className="lead">{message}</p>
+
+            </div>
+
+         </div>
+
+         <div className="row">
+            <div className="eight columns">
+
+               <form action="" method="post" id="contactForm" name="contactForm">
+                  <fieldset>
+
+                     <div>
+                        <label htmlFor="contactName">Name <span className="required">*</span></label>
+                        <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+                     </div>
+
+                     <div>
+                        <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+                        <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+                     </div>
+
+                     <div>
+                        <label htmlFor="contactSubject">Subject</label>
+                        <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+                     </div>
+
+                     <div>
+                        <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                        <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     </div>
+
+                     <div>
+                        <button className="submit">Submit</button>
+                        <span id="image-loader">
+                           <img alt="" src="images/loader.gif" />
+                        </span>
+                     </div>
+                  </fieldset>
+				   </form>
+
+           <div id="message-warning"> Error boy</div>
+				   <div id="message-success">
+                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+				   </div>
+           </div>
+
+
+            <aside className="four columns footer-widgets">
+               <div className="widget widget_contact">
+
+					   <h4>Contact</h4>
+					   <p className="address">
+						   {name}<br />
+						   {city}, {state}<br />
+                     <span>{email}</span><br />
+					   </p>
+				   </div>
+            </aside>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Contact;
+>>>>>>> parent of 7cc863b... installed emailJS and have barebones working contact email working
