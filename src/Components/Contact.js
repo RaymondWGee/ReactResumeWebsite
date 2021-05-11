c9e7d8165789a58bb93d99ccd4ce18fc9c7fc330
Raymond W Gee <rgee5@mail.sfsu.edu>
2021-05-10 21:12:37 -0700

    fixed up styling of contact box with functionality still in tact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -42,60 +42,27 @@ class Contact extends Component {
                <form onSubmit={sendEmail} id="contactForm" name="contactForm">
                   <fieldset>
                      <div>
+                        <label htmlFor="contactName">Name <span className="required">*</span></label>
                         <input type="text" className="form-control" placeholder="Name" name="name"/>
                      </div>
                      <div>
+                        <label htmlFor="contactEmail">Email <span className="required">*</span></label>
                         <input type="email" className="form-control" placeholder="Email Address" name="email"/>
                      </div>
                      <div>
+                        <label htmlFor="contactSubject">Subject</label>
                         <input type="text" className="form-control" placeholder="Subject" name="subject"/>
                      </div>
                      <div>
+                        <label htmlFor="contactMessage">Message <span className="required">*</span></label>
                         <textarea className="form-control" id="" cols="30" rows="8" placeHolder="Your Message" name="message"></textarea>
                      </div>
                      <div>
+                        <label htmlFor="contactMessage"> </label>
                         <input type="submit" className="btn btn-info" value="Send Message"></input>
                      </div>
                   </fieldset>
                </form>
-               {/*
-               <form action="" method="post" id="contactForm" name="contactForm" onSubmit={sendEmail}>
-                  <fieldset>
-
-                     <div>
-                        <label htmlFor="contactName">Name <span className="required">*</span></label>
-                        <input type="text" defaultValue="" size="35" id="name" name="name" onChange={this.handleChange}/>
-                     </div>
-
-                     <div>
-                        <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                        <input type="email" defaultValue="" size="35" id="email" name="email" onChange={this.handleChange}/>
-                     </div>
-
-                     <div>
-                        <label htmlFor="contactSubject">Subject</label>
-                        <input type="text" defaultValue="" size="35" id="subject" name="subject" onChange={this.handleChange}/>
-                     </div>
-
-                     <div>
-                        <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                        <textarea cols="50" rows="15" id="message" name="message"></textarea>
-                     </div>
-
-                     <div>
-                        <button className="submit" value="Send Message">Submit</button>
-                        <span id="image-loader">
-                           <img alt="" src="images/loader.gif" />
-                        </span>
-                     </div>
-                  </fieldset>
-				   </form>
-               */}
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
            </div>
 
             {/* contact section */}
